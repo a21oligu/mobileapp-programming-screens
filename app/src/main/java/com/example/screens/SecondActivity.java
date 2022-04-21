f76a6f2a2142823e0e28f4a69df4de4cee08b7cc
Oliver Gustafsson <oliver.gustafsson22@gmail.com>
2022-04-21 14:42:09 +0200

    data sent with intent is now displayed when SecondActivity is launched

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,15 +3,23 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private TextView textViewDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        textViewDate = findViewById(R.id.textview_date);
+
         Bundle extras = getIntent().getExtras();
-        System.out.println(extras.getString("name"));
+
+        if (extras == null) return;
+
+        textViewDate.setText("Date and time when button was clicked: " + extras.getString("time"));
     }
 }
