38f879c8cd2f0ee671c6abb49780fb4dd8770a6c
Oliver Gustafsson <oliver.gustafsson22@gmail.com>
2022-04-21 14:40:13 +0200

    date at which the button is pressed is now sent with the intent

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent sendIntent;
+    private String currentTimeAndDate;
+    private SimpleDateFormat dateFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dateFormatter = new SimpleDateFormat("dd/MM yyyy - HH:mm.ss", Locale.getDefault());
+
+        sendIntent = new Intent(MainActivity.this, SecondActivity.class);
+
+        sendIntent.setAction(Intent.ACTION_SEND);
+    }
+
+    public void runSecondActivity(View view) {
+        currentTimeAndDate = dateFormatter.format(new Date());
+        sendIntent.putExtra("time", currentTimeAndDate);
+        startActivity(sendIntent);
     }
 }
